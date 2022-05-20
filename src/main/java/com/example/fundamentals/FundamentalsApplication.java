c7d32eece120335d28f13cc622c4fe01eb212491
ts092000 <ts092000@gmail.com>
2022-05-20 09:33:27 +0700

    Chapter 3: Bootstrapping a Simple Application

---
+++ src/main/java/com/example/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,14 @@
+package com.example.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+		System.out.println("Hello pluralsight!");
+	}
+
+}
